df14618b481e80dd4eb03ec853e414061ebe602c
B_Gorkovenko <spirit.wood50@gmail.com>
2019-06-13 20:24:39 +0300

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,6 +1,6 @@
 def mergeSort(alist):
-    print("Splitting ",alist)
-    if len(alist)>1:
+    print("Splitting ", alist)
+    if len(alist) > 1:
         mid = len(alist)//2
         lefthalf = alist[:mid]
         righthalf = alist[mid:]
@@ -8,30 +8,33 @@ def mergeSort(alist):
         mergeSort(lefthalf)
         mergeSort(righthalf)
 
-        leftcounter=0 #for lefthalf
-        rightcounter=0 #for righthalf
-        commoncounter=0 #just counter
-        while leftcounter<len(lefthalf) and rightcounter<len(righthalf):
-            if lefthalf[leftcounter]<righthalf[rightcounter]:
-                alist[commoncounter]=lefthalf[leftcounter]
-                leftcounter+=1
+        leftcounter = 0  # for lefthalf
+        rightcounter = 0  # for righthalf
+        commoncounter = 0  # just counter
+        while leftcounter < len(lefthalf) and rightcounter < len(righthalf):
+            if lefthalf[leftcounter] < righthalf[rightcounter]:
+                alist[commoncounter] = lefthalf[leftcounter]
+                leftcounter += 1
             else:
-                alist[commoncounter]=righthalf[rightcounter]
-                rightcounter+=1
-            commoncounter+=+1
+                alist[commoncounter] = righthalf[rightcounter]
+                rightcounter += 1
+            commoncounter += 1
 
-        while leftcounter<len(lefthalf):
-            alist[commoncounter]=lefthalf[leftcounter]
-            leftcounter+=1
-            commoncounter+=1
+        while leftcounter < len(lefthalf):
+            alist[commoncounter] = lefthalf[leftcounter]
+            leftcounter += 1
+            commoncounter += 1
 
-        while rightcounter<len(righthalf):
-            alist[commoncounter]=righthalf[rightcounter]
-            rightcounter+=1
-            commoncounter+=1
-    print("Merging ",alist)
+        while rightcounter < len(righthalf):
+            alist[commoncounter] = righthalf[rightcounter]
+            rightcounter += 1
+            commoncounter += 1
+    print("Merging ", alist)
+    return alist
 
-alist = [18,35,57,24,
-         68,8,47,25]
-mergeSort(alist)
-print(alist)
+alist = [18, 35, 57, 24, 68, 8, 47, 25]
+
+if __name__ == "__main__":
+    mergeSort(alist)
+    print(alist)
+
